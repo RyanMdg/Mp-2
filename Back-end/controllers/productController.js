5045e9c1bf4bb3d562f25623b14056088bd98b8c
RyanMdg <ryandeguia.dev@gmail.com>
2023-06-23 20:15:39 +0800

    ADDING CONTROLLERS

---
+++ Back-end/controllers/productController.js
@@ -0,0 +1,79 @@
+import prodSchema from "../models/productSchema.js";
+
+// * GET PRODUCTLIST
+const productsList = (req, res) => {
+  prodSchema
+    .find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// * GET SINGLE PRODUCTS
+const productsSingle = (req, res) => {
+  const id = req.params.id;
+
+  prodSchema
+    .findById(id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
+};
+
+// * DELETE PRODUCTS
+const productsDelete = (req, res) => {
+  const id = req.params.id;
+
+  prodSchema
+    .findByIdAndDelete(id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => console.log(err));
+};
+
+// * UPDATE PRODUCTS
+const productsUpdate = (req, res) => {
+  const id = req.params.id;
+  const updatedProducts = req.body;
+
+  const product = prodSchema.find(id);
+
+  prodSchema
+    .findByIdAndUpdate(id, updatedProducts, {
+      new: true,
+    })
+    .then((updatedProducts) => {
+      if (updatedProducts) {
+        res.json({ message: "Product updated successfully" });
+      } else {
+        res.status(404).json({ error: "Product not found" });
+      }
+    })
+    .catch((err) => res.status(404).json({ error: "PRODUCT NOT FIND!" }));
+};
+
+// * ADDED PRODUCTS
+const productsAdded = async (req, res) => {
+  const { productName, price, stock } = req.body;
+
+  try {
+    const products = await prodSchema.create({ productName, price, stock });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export default {
+  productsList,
+  productsSingle,
+  productsDelete,
+  productsUpdate,
+  productsAdded,
+};
--- Back-end/routers/products.js
+++ Back-end/routers/products.js
@@ -1,55 +1,22 @@
 import exprss from "express";
 import prodSchema from "../models/productSchema.js";
+import prodController from "../controllers/productController.js";
 const router = exprss.Router();
 
 //* GET LIST OF PRODUCTS
-router.get("/productlist", (req, res) => {
-  res.json({ mssg: "hello from productlist" });
-});
+router.get("/productlist", prodController.productsList);
 
 //* GET A SINGLE PRODUCTS
-router.get("/productlist/:id", (req, res) => {
-  const id = req.params.id;
-
-  prodSchema
-    .findById(id)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((error) => {
-      res.json({ mssg: error.message });
-    });
-});
+router.get("/productlist/:id", prodController.productsSingle);
 
 //* DELETE PRODUCTS
-router.delete("/productlist/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const prod = await prodSchema.findByIdAndDelete(id);
-    res.status(200).json(prod);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.delete("/productlist/:id", prodController.productsDelete);
 
 //* UPDATE PRODUCTS
-router.patch("/productlist/:id", (req, res) => {
-  res.json({ mssg: "Update a products" });
-});
+router.patch("/productlist/:id", prodController.productsUpdate);
 
 //* POST NEW PRODUCTS
-// * ahahahahah
-// * add add
-router.post("/productadd", async (req, res) => {
-  const { productName, price, stock } = req.body;
 
-  try {
-    const products = await prodSchema.create({ productName, price, stock });
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/productadd", prodController.productsAdded);
 
 export default router;
