4665be4d4575701b5867a6751812e080212bdfd6
RyanMdg <ryandeguia.dev@gmail.com>
2023-07-03 18:46:44 +0800

    deletes

--- Front-End/main-front/pages/cartpage.js
+++ Front-End/main-front/pages/cartpage.js
@@ -6,11 +6,6 @@ const CartPage = ({ cartItems }) => {
         <RiSettings3Fill className="h-24 w-24 animate-spin" />
         <h1>Sorry for the inconvenience, we're still working on our website</h1>
       </div>
-      {cartItems.map((item) => (
-        <div key={item.id}>
-          <p>{item.productName || "Unknown Product"}</p>
-        </div>
-      ))}
     </div>
   );
 };
